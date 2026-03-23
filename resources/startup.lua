64840d9645b126d7c6e61fce91c96a560c2e2dd2
Darianopolis <darianopolis@gmail.com>
2026-03-23 15:59:50 +0000

    Update default config

--- resources/startup.lua
+++ resources/startup.lua
@@ -2,14 +2,13 @@ source("config.lua")
 
 -- XWayland --------------------------------------------------------------------
 
-xwayland(nil, function(socket) env.DISPLAY     = socket end)
+xwayland(nil, function(socket) env.DISPLAY = socket end)
 xwayland(nil, function(socket) env.ALT_DISPLAY = socket end)
 
 -- Clients ---------------------------------------------------------------------
 
 spawn("dunst")
 spawn("blueman-applet")
-spawn("1password", "--silent")
 spawn("waybar")
 spawn("playerctld")
 spawn("wl-paste", "--watch", "cliphist", "store")
@@ -18,9 +17,12 @@ spawn("wl-paste", "--watch", "cliphist", "store")
 
 config.background.image = env.WALLPAPER
 
+local primary   = "DP-1"
+local secondary = "DP-2"
+
 config.output.on_add_or_remove = function(output, added)
-    spawn("wlr-randr", "--output", "DP-1", "--adaptive-sync", "disabled", "--output", "DP-2", "--left-of", "DP-1")
+    spawn("wlr-randr", "--output", secondary, "--left-of", primary)
 end
 
-config.bind["Mod+p"]       = function() spawn("wlr-randr", "--output", "DP-1", "--adaptive-sync", "enabled")  end
-config.bind["Mod+Shift+P"] = function() spawn("wlr-randr", "--output", "DP-1", "--adaptive-sync", "disabled") end
+config.bind["Mod+p"]       = function() spawn("wlr-randr", "--output", primary, "--adaptive-sync", "enabled")  end
+config.bind["Mod+Shift+P"] = function() spawn("wlr-randr", "--output", primary, "--adaptive-sync", "disabled") end
